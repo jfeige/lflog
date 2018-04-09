0d1b480bdbd660f92c1873bc56b27cee68ea36d4
飞 <345364004@qq.com>
2018-04-09 17:35:14 +0800

    Add files via upload
    按照自然日期，自动拆分，备份日志

--- logrecord.go
+++ logrecord.go
@@ -29,7 +29,7 @@ type LogFile struct {
 	Daily    bool
 }
 
-func (l LogRecord) write(source, message string) {
+func (l *LogRecord) write(source, message string) {
 	//组装字符串，写入队列
 	//[2018/04/03 15:07:44 CST] [INFO] [action.addDynamic:230] addDynamic--------------%!(EXTRA string=110, int=1583649)
 	date := time.Now().Format("2006-01-02 15:04:05")
@@ -43,34 +43,44 @@ func (l LogRecord) write(source, message string) {
 }
 
 //info
-func (l LogRecord) writeLog() {
+func (l *LogRecord) writeLog() {
 	for {
 		select {
 		case message := <-l.MessageQueue:
 			//写日志文件
-			l.checkLogDate(l.Logfile.Filename)
-			fmt.Fprintln(l.f,message)
+			l.checkLogDate()
+			_,err := fmt.Fprintln(l.f,message)
 		}
 	}
 }
 
 //写日志时，判断是否已跨日，如果已跨日，则备份日志
-func (l LogRecord) checkLogDate(filename string){
+func (l *LogRecord) checkLogDate(){
 	if time.Now().Format("2006-01-02") != l.Opendate{
 		var err error
 		var num = 1
 		//当前日期和程序建立时的日期不符，将日志备份为昨天的日期
 		yesterday := time.Now().AddDate(0, 0, -1).Format("2006-01-02")
+		//重命名带 1，2，3后缀的日志文件
 		for ;err == nil && num < 999;num++{
 			new_name := l.Logfile.Filename + fmt.Sprintf(".%s.%03d",yesterday,num)
 			old_name := l.Logfile.Filename + fmt.Sprintf(".%d",num)
 			_,err = os.Lstat(old_name)
 			if err == nil{
 				os.Rename(old_name,new_name)
+			}else{
+				break
 			}
 		}
+		//关闭当前
+		l.f.Close()
+		_,err = os.Lstat(l.Logfile.Filename)
+		if err == nil{
+			new_name := l.Logfile.Filename + fmt.Sprintf(".%s.%03d",yesterday,num)
+			os.Rename(l.Logfile.Filename,new_name)
 
-		l.f,err = os.OpenFile(filename,os.O_WRONLY|os.O_APPEND,0666)
+		}
+		l.f,err = os.OpenFile(l.Logfile.Filename,os.O_APPEND|os.O_WRONLY|os.O_CREATE,0666)
 	}
 }
 
@@ -87,11 +97,14 @@ func createLogFile(filename string){
 	for ;err == nil && num <= 999;num++{
 		fname = filename + fmt.Sprintf(".%d",num)
 		_,err = os.Lstat(fname)
+		if err != nil{
+			//更改日志文件名称
+			os.Rename(filename,fname)
+			os.Create(filename)
+			break
+		}
 	}
 	if err == nil{
 		//当天已到999个日志，抛出异常
 	}
-	//更改日志文件名称
-	os.Rename(filename,fname)
-	os.Create(filename)
 }
